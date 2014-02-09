6bbdf30d6cb7167d37f2fad823d43e13b5d44473
Elwira Lichocka <ariwle.lichocka@gmail.com>
2014-02-10 00:08:38 +0100

    na razie wykrywanie wstrząsu

---
+++ PhoneApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace PhoneApp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        private ShakeDetector _shakeDecetor;
+        public MainPage()
+        {
+            InitializeComponent();
+            _shakeDecetor = new ShakeDetector();
+            _shakeDecetor.ShakeEvent += new EventHandler<EventArgs>(_shakeDecetor_ShakeEvent);
+            _shakeDecetor.Start();
+        }
+
+        void _shakeDecetor_ShakeEvent(object sender, EventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(() =>
+                                            {
+                                                Storyboard shakeAnimation = Resources["ShakeAnimation"] as Storyboard;
+                                                shakeAnimation.Begin();
+                                            });
+            
+        }
+    }
+}
